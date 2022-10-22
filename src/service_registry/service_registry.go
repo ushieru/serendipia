ac94ad788d0b4c394ee302363203b1ac9ef858db
ushieru <uzielcocolan@gmail.com>
2022-10-22 00:23:17 -0500

    Fix name Add Init method

--- src/service_registry/service_registry.go
+++ src/service_registry/service_registry.go
@@ -6,47 +6,54 @@ import (
 )
 
 type ServiceRegistry struct {
-	services  map[string]service
-	heartBeat int64
-	handlers  []func()
+	Services  map[string]Service
+	HeartBeat int64
+	Handlers  []func()
 }
 
 func (serviceRegistry *ServiceRegistry) SetHandler(handler func()) {
-	serviceRegistry.handlers = append(serviceRegistry.handlers, handler)
+	serviceRegistry.Handlers = append(serviceRegistry.Handlers, handler)
 }
 
 func (serviceRegistry *ServiceRegistry) Register(name string, ip string, port string) string {
 	key := name + ip + port
 	now := time.Now().UTC().UnixNano() / int64(time.Millisecond)
-	if val, ok := serviceRegistry.services[key]; ok {
-		val.timestamp = now
+	if val, ok := serviceRegistry.Services[key]; ok {
+		val.Timestamp = now
 		fmt.Printf("[ServiceRegistry] Updated service: %s at %s:%s", name, ip, port)
 		return key
 	}
-	service := service{
-		name:      name,
-		ip:        ip,
-		port:      port,
-		timestamp: now,
+	service := Service{
+		Name:      name,
+		Ip:        ip,
+		Port:      port,
+		Timestamp: now,
 	}
-	serviceRegistry.services[key] = service
+	serviceRegistry.Services[key] = service
 	fmt.Printf("[ServiceRegistry] Registered service: %s at %s:%s", name, ip, port)
 	return key
 }
 
 func (serviceRegistry *ServiceRegistry) Unregister(name string, ip string, port string) {
 	key := name + ip + port
-	delete(serviceRegistry.services, key)
+	delete(serviceRegistry.Services, key)
 	print("[ServiceRegistry] Unregister service: %s at %s:%s", name, ip, port)
 }
 
 func (serviceRegistry *ServiceRegistry) Cleanup() {
 	now := time.Now().UTC().UnixNano() / int64(time.Millisecond)
-	for key, service := range serviceRegistry.services {
-		willBeEliminated := now-service.timestamp > int64(serviceRegistry.heartBeat)
+	for key, service := range serviceRegistry.Services {
+		willBeEliminated := now-service.Timestamp > int64(serviceRegistry.HeartBeat)
 		if willBeEliminated {
-			delete(serviceRegistry.services, key)
+			delete(serviceRegistry.Services, key)
 			print("[ServiceRegistry] Eliminating service: %s", key)
 		}
+	}
+}
+
+func (serviceRegistry *ServiceRegistry) Init() {
+	for {
+		serviceRegistry.Cleanup()
+		time.Sleep(time.Duration(serviceRegistry.HeartBeat) * time.Second)
 	}
 }
