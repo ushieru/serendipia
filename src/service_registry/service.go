9da5f98fc55bc45e41b819dd0ead664220694283
ushieru <uzielcocolan@gmail.com>
2022-10-23 15:01:54 -0500

    Fix add json

--- src/service_registry/service.go
+++ src/service_registry/service.go
@@ -1,9 +1,9 @@
 package serviceregistry
 
 type Service struct {
-	Name      string
-	Ip        string
-	Protocol  string
-	Port      string
-	Timestamp int64
+	Name      string `json:"name"`
+	Ip        string `json:"ip"`
+	Protocol  string `json:"protocol"`
+	Port      string `json:"port"`
+	Timestamp int64  `json:"timestamp"`
 }
